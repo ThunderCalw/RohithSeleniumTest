a66abbeb6841ec7168c8973c9f3fb072352c13f6
Rohith Raja <50719490+ThunderCalw@users.noreply.github.com>
2025-01-11 15:01:42 +0530

    Create datatypes.java

---
+++ week1/HomeAssignment/datatypes.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 1998;
+		char shortcutKey = 'C';
+		
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortcutKey);
+	}
+
+}

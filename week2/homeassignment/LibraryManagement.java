a48c39c4eeb85fa4f70bf4d1a19be4a14335aeb8
Rohith Raja <50719490+ThunderCalw@users.noreply.github.com>
2025-01-18 14:56:51 +0530

    Create LibraryManagement.java

---
+++ week2/homeassignment/LibraryManagement.java
@@ -0,0 +1,12 @@
+package homeassignments;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Library obj1 = new Library();
+		obj1.addBook("Book");
+		obj1.issueBook();
+	}
+	
+}
